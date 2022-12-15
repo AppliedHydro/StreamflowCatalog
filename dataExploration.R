1fb5f6e894a234f3e7a4dc2561ec2a38c82f4cf7
Kendra Kaiser <kendrakaiser@boisestate.edu>
2022-12-15 10:20:03 -0700

    Additional filtering added

--- dataExploration.R
+++ dataExploration.R
@@ -3,3 +3,20 @@
 # August 26th, 2021
 # Project funded by the Northwest Climate Adaptation Science Center
 
+library(readxl)
+library(tidyverse)
+
+setwd("/Users/kendrakaiser/github/StreamflowCatalog")
+#USFS Region 6 stream 
+catalog<- read_excel("data/Streamflow_Catalog.xlsx")
+cat_new<- catalog %>% filter(organization != "United States Geological Survey")
+
+cont<- cat_new  %>% filter(meas.freq  == 'continuous')
+syn<- cat_new  %>% filter(meas.freq  == 'synoptic')
+seas<- cat_new  %>% filter(meas.freq  == 'seasonal')
+unk<- cat_new  %>% filter(meas.freq  == 'unknown')
+naa<- cat_new  %>% filter(meas.freq  == 'NA')
+
+ID_cont <- cat_new %>% filter(meas.freq  == 'continuous') %>% filter(state == "Idaho")
+
+unique_co<- unique(catalog$county)
