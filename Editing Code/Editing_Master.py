172fc368e1e5c01ff21600f33a69c669712a59ac
Steven Schmitz <120681809+sjschmitz@users.noreply.github.com>
2023-09-12 11:50:32 -0600

    Update Editing_Master.py
    Fixed streamtype code to be able to actively subset by state/streamtype. Turned into function notation.

--- Editing Code/Editing_Master.py
+++ Editing Code/Editing_Master.py
@@ -13,9 +13,10 @@ import matplotlib.pyplot as plt   #visualizes data
 import numpy as np                #mathematical library with built-in calculation tools
 import matplotlib as mpl
 
-w_dir = 'C:/Users/sjsch/Desktop/Kendra/Streamflow_catalog.xlsx' #streamflow catalog pathway
+w_dir = 'C:/Users/sjsch/Desktop/Kendra/Streamflow_Catalog.xlsx' #streamflow catalog pathway
 workbook = px.open(w_dir)
 wb = workbook.active
+maxrow = wb.max_row
 
 #------------------------------------------------------
 #checking for streamflow catalog length
@@ -26,7 +27,6 @@ def get_length(wb):
             index_row.append(i)
     for row_del in range(len(index_row)):
         ws.delete_rows(idx=index_row[row_del], amount=1)
-    maxrow = wb.max_row
     print(f"Streamflow catalog is {maxrow} lines")
     return maxrow
 
@@ -35,7 +35,7 @@ def get_length(wb):
 #------------------------------------------------------
 
 def year_editor():
-    err_msg = 'incorrect year values'
+    err_msg = 'incorrect year format values'
     counter = 0
     for rows in wb.iter_rows(min_row=1,max_row=12671,min_col=1,max_col=1): #gage start dates
         for cell in rows:
@@ -43,28 +43,27 @@ def year_editor():
             date = str(cell.value)wb.cell(34406,1)
             assert len(date) == 4,err_msg
             cell.value = ('1/1/'+date)
-    
-counter = 0
-for rows in wb.iter_rows(min_row=1,max_row=12671,min_col=2,max_col=2): #gage end dates
-    for cell in rows:
-        counter += 1
-        date = str(cell.value)
-        cell.value = ('1/1/'+date)
-
-workbook.save('C:/Users/sjsch/Desktop/Kendra/brpdates.xlsx')
+    counter = 0
+    for rows in wb.iter_rows(min_row=1,max_row=12671,min_col=2,max_col=2): #gage end dates
+        for cell in rows:
+            counter += 1
+            date = str(cell.value)
+            cell.value = ('1/1/'+date)
+    workbook.save('C:/Users/sjsch/Desktop/Kendra/Streamflow_catalog.xlsx')
 
 #------------------------------------------------------
 #checking for empty latitude/longitude fields
 #------------------------------------------------------
-counter = 1
-checks = [] #this list will have the row numbers of any empties
-for rows in wb.iter_rows(min_row=2,max_row=maxrow,min_col=11,max_col=11):
-    for cell in rows:
-        counter += 1
-        if str(cell.value).lower() == 'none':
-            checks.append(counter)
-        else:
-            pass
+def empty_latlon():
+    counter = 1 #skips header
+    checks = [] #this list will have the row numbers of any empties
+    for rows in wb.iter_rows(min_row=2,max_row=maxrow,min_col=11,max_col=11):
+        for cell in rows:
+            counter += 1
+            if str(cell.value).lower() == 'none':
+                checks.append(counter)
+            else:
+                pass
 
 #------------------------------------------------------
 #checking for empty date fields/erroneous default values
@@ -108,43 +107,56 @@ except:             #handles any exceptions outside of parameters
     raise
 
 #------------------------------------------------------
-#checking for streamtype values outside of standard
+#checking for streamtype values subset by state
 #------------------------------------------------------
+
+import openpyxl as px
+import operator as op
+from collections import Counter
+
+w_dir = 'C:/Users/sjsch/Desktop/Kendra/Streamflow_Catalog.xlsx' #streamflow catalog pathway
+workbook = px.open(w_dir)
+wb = workbook.active
+maxrow = wb.max_row
+
+#Creates a 'streamtype' class where state,stream types can be easily accessed
+class streamtype:
+    def __init__(self,stream,state):
+        self.stream = stream
+        self.state = state
+
+#checks all unique values in 'stream type' column for referencing
+unique = []   
+for row in wb.iter_rows(min_col=9,max_col=9,min_row=2,max_row=maxrow): #row 9 = streamtype
+    for cell in row:
+        cell.value = str(cell.value).rstrip() #remove problematic white spaces
+        if str(cell.value) not in unique:
+            unique.append(str(cell.value))
+            continue
+        elif str(cell.value) in unique:
+            pass
+
+type_data = []
+counter = 1
 for row in wb.iter_rows(min_col=9,max_col=9,min_row=2,max_row=maxrow):
     for cell in row:
+        type_data.append(streamtype(wb.cell(counter,9).value,wb.cell(counter,6).value))
         counter += 1
-        var = str(cell.value).lower()
-        var = var.rstrip(' ')
-        if str(wb.cell(counter,21).value) == 'continuous': #subset by continuous
-            if str(wb.cell(counter,1).value) == 'None':
-                continue
-            elif var == 'none' or var == 'unknown':
-                if wb.cell(counter,6).value == I:
-                    ID_unk.append(var)
-                elif wb.cell(counter,6).value == O:
-                    OR_unk.append(var)
-                elif wb.cell(counter,6).value == W:
-                    WA_unk.append(var)
-            elif var == 'canal':
-                if wb.cell(counter,6).value == I:
-                    ID_canal.append(var)
-                elif wb.cell(counter,6).value == O:
-                    OR_canal.append(var)
-                elif wb.cell(counter,6).value == W:
-                    WA_canal.append(var)
-            elif var == 'stream':
-                if wb.cell(counter,6).value == I:
-                    ID_stream.append(var)
-                elif wb.cell(counter,6).value == O:
-                    OR_stream.append(var)
-                elif wb.cell(counter,6).value == W:
-                    WA_stream.append(var)
-            elif var == 'weir':
-                if wb.cell(counter,6).value == I:
-                    ID_weir.append(var)
-                elif wb.cell(counter,6).value == O:
-                    OR_stream.append(var)
-                elif wb.cell(counter,6).value == W:
-                    WA_stream.append(var)
-        else:
-            exceptions.append(str(counter)+ var)
+type_data = list(filter(lambda item: item.state is not None, type_data))
+
+def stream_counter(type_data):
+    """
+    Using the stream_counter function, you can pass state variables to it and it will 
+    parse number of streams of each type subset by state.
+    @type_data : list of streamtype class objects with self.stream, self.state attributes
+    """
+    usr_state = input('input full state name (ex. Idaho): ')
+    stream_total = []
+    for val in type_data:
+        if usr_state == val.state:
+            stream_total.append(val)
+    print(len(stream_total))
+    strm = sum(val.stream == 'stream' for val in stream_total)
+    canl = sum(val.stream == 'canal' for val in stream_total)
+    weir = sum(val.stream == 'weir' for val in stream_total)
+    print(f'There are: \na. {strm} streams\nb. {canl} canals\nc. {weir} weirs')
